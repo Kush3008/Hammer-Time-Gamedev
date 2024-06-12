d9d4f2d5fbcf279ef8c420427051660f10876055
Kushagra <83759659+Kush3008@users.noreply.github.com>
2024-06-13 00:51:04 +0530

    Update Collision.js

--- js/classes/Collision.js
+++ js/classes/Collision.js
@@ -6,28 +6,10 @@ class CollisionBlock {
     }
 
     draw() {
-        c.fillStyle = 'rgba(255, 0, 0, 0.5)'
+        c.fillStyle = 'rgba(255, 0, 0, 0.5)';
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
 
-Array.prototype.createObjectsFrom2D = function () {
-    const objects = []
-    this.forEach((row, y) => {
-    row.forEach((symbol, x) => {
-        if (symbol === 292 || symbol === 250) {
-            objects.push(
-                new CollisionBlock({
-                position: {
-                    x: x * 64,
-                    y: y * 64,
-                },
-            })
-        )
-    }
-
-    })
-})
-
-    return objects
-}
+// Export to global scope
+window.CollisionBlock = CollisionBlock;
