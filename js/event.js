094206260b98d399549a0aa0f0af7a8a007a360c
Kushagra <83759659+Kush3008@users.noreply.github.com>
2024-06-13 01:22:05 +0530

    Update event.js

--- js/event.js
+++ js/event.js
@@ -8,6 +8,35 @@ window.addEventListener('keydown', (event) => {
     case 'w':
       actionSequence.push('up'); // Track 'up' action
       if (player.velocity.y === 0) player.velocity.y = -20; // Jump action
+
+      for (let i = 0; i < doors.length; i++) {
+        const door = doors[i];
+
+        if (
+          player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width &&
+          player.hitbox.position.x >= door.position.x &&
+          player.hitbox.position.y + player.hitbox.height >= door.position.y &&
+          player.hitbox.position.y <= door.position.y + door.height
+        ) {
+          player.velocity.x = 0;
+          player.velocity.y = 0;
+          player.preventInput = true;
+          player.switchSprite('enterDoor');
+          door.play();
+
+          setTimeout(() => {
+            if (i === 0) {
+              window.location.href = 'https://www.linkedin.com/in/kushagra-agarwal-88614b219/';
+            } else if (i === 1) {
+              window.location.href = 'https://github.com/Kush3008';
+            } else if (i === 2) {
+              window.location.href = 'https://www.behance.net/Kush3008';
+            }
+          }, 1000); // Delay for animation
+
+          return;
+        }
+      }
       break;
     case 'a':
       actionSequence.push('left'); // Track 'left' action
@@ -28,35 +57,6 @@ window.addEventListener('keydown', (event) => {
   if (JSON.stringify(actionSequence) === JSON.stringify(requiredSequence)) {
     window.location.href = 'https://drive.google.com/file/d/1py7ypsmaevFG1JMuugt57QmmQCeXQtuq/view?usp=sharing'; // Redirect to secret website
     actionSequence.length = 0; // Reset sequence after successful activation
-  }
-
-  for (let i = 0; i < doors.length; i++) {
-    const door = doors[i];
-
-    if (
-      player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width &&
-      player.hitbox.position.x >= door.position.x &&
-      player.hitbox.position.y + player.hitbox.height >= door.position.y &&
-      player.hitbox.position.y <= door.position.y + door.height
-    ) {
-      player.velocity.x = 0;
-      player.velocity.y = 0;
-      player.preventInput = true;
-      player.switchSprite('enterDoor');
-      door.play();
-
-      setTimeout(() => {
-        if (i === 0) {
-          window.location.href = 'https://www.linkedin.com/in/kushagra-agarwal-88614b219/';
-        } else if (i === 1) {
-          window.location.href = 'https://github.com/Kush3008';
-        } else if (i === 2) {
-          window.location.href = 'https://www.behance.net/Kush3008';
-        }
-      }, 1000); // Delay for animation
-
-      return;
-    }
   }
 });
 
